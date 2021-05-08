f1369d1c70ee849abe9d2d84c26351f1dffb9c42
rhjje <numelen@gmail.com>
2021-05-08 13:44:19 +0300

    fix: fixed eslint bugs

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -6,21 +6,21 @@ const keyboard = document.querySelector('.keyboard');
 const textarea = document.querySelector('.textarea');
 const main = document.querySelector('.main');
 
-document.querySelector('div.dark-theme > label > input[type=checkbox]').addEventListener('change', (event) => {
-    keys.forEach(key => {
-        key.classList.toggle('dark-theme-keys');
-    });
+document.querySelector('div.dark-theme > label > input[type=checkbox]').addEventListener('change', () => {
+  keys.forEach((key) => {
+    key.classList.toggle('dark-theme-keys');
+  });
 
-    side.forEach(key => {
-        key.classList.toggle('dark-theme-side-key');
-    });
+  side.forEach((key) => {
+    key.classList.toggle('dark-theme-side-key');
+  });
 
-    sideRus.forEach(key => {
-        key.classList.toggle('dark-theme-side-key');
-    });
+  sideRus.forEach((key) => {
+    key.classList.toggle('dark-theme-side-key');
+  });
 
-    keyboard.classList.toggle('dark-theme-keyboard');
-    body.classList.toggle('dark-theme-bg');
-    main.classList.toggle('dark-theme-main');
-    textarea.classList.toggle('dark-theme-textarea');
+  keyboard.classList.toggle('dark-theme-keyboard');
+  body.classList.toggle('dark-theme-bg');
+  main.classList.toggle('dark-theme-main');
+  textarea.classList.toggle('dark-theme-textarea');
 });
--- src/js/speech.js
+++ src/js/speech.js
@@ -1,35 +1,35 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-let rec = new SpeechRecognition();
+const rec = new SpeechRecognition();
 rec.interimResults = true;
 rec.continuous = true;
-rec.lang = 'en-gb'; 
+rec.lang = 'en-gb';
 
 document.querySelector('div.key.win').addEventListener('click', () => {
-    if (languageEnglish) {
-        rec.lang = 'en-gb';
-    } else {
-        rec.lang = 'ru';
-    }
+  if (languageEnglish) {
+    rec.lang = 'en-gb';
+  } else {
+    rec.lang = 'ru';
+  }
 });
 
-rec.addEventListener("result", function(e) {
-    let text = Array.from(e.results)
+rec.addEventListener('result', () => {
+  const text = Array.from(e.results)
     .map(result => result[0])
     .map(result => result.transcript)
     .join('');
-    textarea.value = text;
+  textarea.value = text;
 });
 
 const microphoneButton = document.querySelector('.microphone > img');
 microphoneButton.addEventListener('click', () => {
-    if (microphoneButton.classList.contains('off')) {
-        microphoneButton.src = 'assets/icons/microphone.svg';
-        microphoneButton.classList.remove('off');
-        rec.start();
-    } else {
-        rec.stop();
-        microphoneButton.src = 'assets/icons/microphone-off.svg';
-        microphoneButton.classList.add('off');
-    }
+  if (microphoneButton.classList.contains('off')) {
+    microphoneButton.src = 'assets/icons/microphone.svg';
+    microphoneButton.classList.remove('off');
+    rec.start();
+  } else {
+    rec.stop();
+    microphoneButton.src = 'assets/icons/microphone-off.svg';
+    microphoneButton.classList.add('off');
+  }
 });
